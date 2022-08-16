3a4b8c2a2835c5187748df732ff481e1651d801e
Antoine BACK <antoine.back@hotmail.fr>
2022-08-16 18:21:04 +0200

    Rank channels by number of viewers

--- app.py
+++ app.py
@@ -28,23 +28,28 @@ class Channel:
         self.image: str = ""
         self.offline_image: str = ""
         self.game: str = ""
-        self.viewer: str = ""
+        self.viewer: int = 0
+        self.error: bool = False
 
     def __str__(self):
         """
-        This function returns a string that contains the login,
-        name, id, live status, image, and game of the channel
+        The __str__ function is a special function
+        that is called when you print an object
+
+        :return: A string with the channel name, whether
+        it is live, and the number of viewers.
         """
         return (
-            f"Channel :\n\tLogin: {self.login}\n\tName: {self.name}\n\t"
-            f"Id: {self.channel_id}\n\tLive: {self.live}\n\t"
-            f"Image: {self.image}\n\tGame: {self.game}\n"
+            f"Channel :\n\tName: {self.name}\n\t"
+            f"Live: {self.live}\n\tViewers: {self.viewer}"
         )
 
     def get_channel_info(self) -> int:
         """
-        It gets the channel's information from the Twitch API and
-        stores it in the class
+        It gets the channel's information from the
+        Twitch API and stores it in the class
+
+        :return: The return value is an integer for errors.
         """
         try:
             response = rq.get(
@@ -72,10 +77,10 @@ class Channel:
 
     def islive(self) -> int:
         """
-        It checks if the user is live, and if they are, it sets the
-        `live` attribute to `True`, and sets the `game` and `viewer`
-        attributes to the game they're playing and the number of viewers
-        they have, respectively
+        It checks if the channel is live and if it is,
+        it updates the game and viewer count
+
+        :return: The return value is the status code of the request.
         """
         if self.channel_id is None:
             return 1
@@ -100,30 +105,35 @@ class Channel:
         if len(jsn["data"]) > 0:
             self.live = True
             self.game = jsn["data"][0]["game_name"]
-            self.viewer = re.sub(
-                r"([0-9,]*)([KM])", r"\1&nbsp;\2",
-                numerize.numerize(jsn["data"][0]["viewer_count"])
-            )
-            # self.viewer = numerize.numerize(jsn["data"][0]["viewer_count"])
+            self.viewer = int(jsn["data"][0]["viewer_count"])
         return 0
 
     def tohtml(self) -> str:
         """
         It returns a string that contains the HTML code of the streamer's card
+
         :return: A string
         """
-        witchgame: str = f"<p>{self.game}</p>" if self.live else ""
-        islive: str = (
-            f"<p class='live'>&#x2022;&nbsp;</p><p>{self.viewer}</p>"
-            if self.live
-            else "<p class='disconnected'>Déconnecté(e)</p>"
-        )
+        if self.live:
+            nbviewers = re.sub(
+                r"([0-9,]*)([KM])",
+                r"\1&nbsp;\2",
+                numerize.numerize(self.viewer, 1)
+            ).lower()
+            witchgame: str = f"<p>{self.game}</p>"
+            islive: str = (
+                "<p class='live'>&#x2022;&nbsp;</p>"
+                f"<p>{nbviewers}</p>"
+            )
+        else:
+            witchgame: str = ""
+            islive: str = "<p class='disconnected'>Déconnecté(e)</p>"
         return f"""
 <div class="main">
     <a href="https://www.twitch.tv/{self.login}">
         <div class="frame">
             <img
-                style="filter: grayscale({0 if self.live else 1});"
+                style="filter: grayscale({"0" if self.live else "100%"});"
                 src="{self.image}"
             />
             <div class="text">
@@ -139,21 +149,30 @@ class Channel:
 
 def main():
     """
-    It reads the channels.txt file, creates a Channel object for each line,
-    gets the channel info, checks if the channel is live, and then writes
-    the HTML for each channel to stdout
+    It reads the channels.txt file, gets the channel info
+    and live status for each channel, sorts them by viewer
+    count, and outputs the HTML
+
+    :return: the html code for the widget.
     """
     try:
         rq.get('https://google.com')
     except rq.exceptions.RequestException:
-        sys.stdout.write("<div class='error'>&#9888;&nbsp;Not connected</div>")
+        sys.stdout.write("<div class='error'>"
+                         "&#9888;&nbsp;Not connected.</div>")
         return
 
     channels = []
     with open("Twitch.widget/channels.txt", encoding="utf-8") as file:
         channels.extend(Channel(line.strip()) for line in file)
+
     for channel in channels:
-        if (channel.get_channel_info() or channel.islive()):
+        channel.error = channel.get_channel_info() or channel.islive()
+
+    channels.sort(key=lambda x: x.viewer, reverse=True)
+
+    for channel in channels:
+        if channel.error:
             sys.stdout.write("<div class='error'>An error has occurred.</div>")
         else:
             sys.stdout.write(channel.tohtml())
